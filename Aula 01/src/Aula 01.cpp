c5bd5dde25d79fd57b42baa6362762af671807bd
Rodrigo Batista <rodrigo.s.batista@outlook.com>
2021-07-20 22:03:05 -0300

    iniciando a 1ª aula no Eclipse

---
+++ Aula 01/src/Aula 01.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Aula.cpp
+// Author      : Rodrigo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}

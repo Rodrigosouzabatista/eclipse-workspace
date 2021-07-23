b2a1a0a795066af39285db0b408efbda714eb859
Rodrigo Batista <rodrigo.s.batista@outlook.com>
2021-07-22 21:24:12 -0300

    essage

---
+++ Aula 03/Principal.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+//recebe zero ou mais valores
+//retorna um unico valor
+double soma (double a, double b){
+	return a + b;
+}
+double mult(double a,double b){
+	return a * b;
+}
+double mult (double a, double b, double c){
+	return a * b * c;
+}
+
+
+
+int main(int argc, char **argv) {
+
+	double numeroA;
+	double numeroB;
+
+    std::cin >> numeroA;
+    std::cin >> numeroB;
+
+	std::cout << soma(numeroA, numeroB) << std::endl;
+}
+
+
